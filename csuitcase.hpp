bc91b2b66bb0a39609e0a6362831bb18980a1e6c
Miro Hrončok <miro@hroncok.cz>
2014-02-22 21:46:27 +0100

    Documented CStuff abstract class

--- csuitcase.hpp
+++ csuitcase.hpp
@@ -5,17 +5,35 @@
 #include <string.h>
 using namespace std;
 
+/// Stuff you can put to a suitcase
+/** This abstract class represents anything that can be put into a suitcase.
+ *  It stores it's weight and information, if the thing is dangerous or not. */
 class CStuff {
 protected:
 	bool danger;
 	int weight;
 public:
+	/// Implicit constructor
 	CStuff();
+	/// Destructor
 	virtual ~CStuff() {};
+	/// Gets dangerousness
+	/** \return True if the stuff is dangerous */
 	virtual bool Danger() const;
+	/// Gets weight
+	/** \return Weight of the stuff in grams */
 	virtual int Weight() const;
+	/// Gets the number of stuff in this stuff
+	/** Implicitly each stuff is one stuff.
+	 *  But sometimes a stuff can contain another stuff.
+	 *  \return Number of stuff in this stuff (counting itself as well) */
 	virtual int Count() const;
+	/// Gets a textual representation of this stuff
+	/** For basic stuff, this a one-line text describing the thing.
+	 *  If the stuff contains more stuff, it is displayed as a tree. */
 	virtual string to_string(string) const = 0;
+	/// Cloner
+	/** \return Deep copy of the stuff */
 	virtual CStuff * clone() const = 0;
 };
 
